d9afd9c055f2abb212a83e779b41d22a8b033038
Lucas Kahlert <lucas.kahlert@cloudandheat.com>
2019-07-24 13:45:09 +0200

    Add docstring for rok module

---
+++ rok/rok/__init__.py
@@ -0,0 +1,4 @@
+""":mod:`rok` is a Python command line interface for the :mod:`krake` API
+server. It can be used to manipulate any RESTful resource handled by Krake. It
+can be used by end users as well as for administrative tasks.
+"""
