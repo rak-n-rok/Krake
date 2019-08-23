686ff7e3cb7421c372d758782f65e133e944487b
Jean Chorin <jean.chorin@cloudandheat.com>
2019-08-23 16:52:44 +0200

    Correct etcd race condition for tests
    See issue #244
    
    To ensure that the etcd database is started, the fixture 'db' has
    been added to the test with race condition. The fixture blocks if
    the database is started, and the test can only start when the fixture
    finished.

--- krake/tests/api/test_app.py
+++ krake/tests/api/test_app.py
@@ -2,7 +2,7 @@ from krake.api import __version__ as version
 from krake.api.app import create_app
 
 
-async def test_index(aiohttp_client, config):
+async def test_index(aiohttp_client, db, config):
     client = await aiohttp_client(create_app(config=config))
     resp = await client.get("/")
     assert resp.status == 200
