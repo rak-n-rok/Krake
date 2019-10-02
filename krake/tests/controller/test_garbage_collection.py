f4b1808506826c6c30c37e32675be933ca87fab1
Jean Chorin <jean.chorin@cloudandheat.com>
2019-10-02 17:01:13 +0200

    Refactor the GC with the new Controller API
    To use the newly refactored Controller, the garbage collector have been
    modified. It does not have any worker anymore, but now uses a special
    reflector to connect to the database. One reflector is used for each
    resource that needs to be garbage collected.
    
    See issue #259
    
    Signed-off-by: Jean Chorin <jean.chorin@cloudandheat.com>

--- krake/tests/controller/test_garbage_collection.py
+++ krake/tests/controller/test_garbage_collection.py
@@ -1,19 +1,15 @@
-import asyncio
 import pytz
 
-from krake.api.app import create_app
+from krake.api.database import Session
 from krake.data.core import resource_ref
 from krake.data.kubernetes import Application, ApplicationState, ClusterState, Cluster
-from krake.controller import Worker
-from krake.controller.garbage_collector import GarbageCollector, GarbageWorker
-from krake.client import Client
-from krake.test_utils import server_endpoint
+from krake.controller.garbage_collector import GarbageCollector
 
 from factories.fake import fake
 from factories.kubernetes import ApplicationFactory, ClusterFactory
 
 
-async def test_resources_reception(aiohttp_server, config, db, loop):
+async def test_resources_reception(config, db, loop):
     app_updated = ApplicationFactory(status__state=ApplicationState.UPDATED)
     app_deleting = ApplicationFactory(
         status__state=ApplicationState.RUNNING,
@@ -26,42 +22,28 @@ async def test_resources_reception(aiohttp_server, config, db, loop):
         metadata__deleted=fake.date_time(tzinfo=pytz.utc),
     )
 
-    server = await aiohttp_server(create_app(config))
+    await db.put(app_updated)
+    await db.put(app_deleting)
 
-    class SimpleWorker(Worker):
-        def __init__(self):
-            self.done = loop.create_future()
+    await db.put(cluster_updated)
+    await db.put(cluster_deleting)
 
-        async def resource_received(self, resource):
-            assert resource in [app_deleting, cluster_deleting]
+    gc = GarbageCollector(worker_count=0, db_host=db.host, db_port=db.port)
+    gc.create_background_tasks()  # need to be called explicitly
+    async with Session(host=gc.db_host, port=gc.db_port) as session:
+        for reflector in gc.reflectors:
+            await reflector.list_resource(session)
 
-            if not self.done.done():
-                self.done.set_result(None)
+    assert gc.queue.size() == 2
+    key_1, value_1 = await gc.queue.get()
+    key_2, value_2 = await gc.queue.get()
 
-    worker = SimpleWorker()
-
-    async with GarbageCollector(
-        api_endpoint=server_endpoint(server),
-        worker_factory=lambda client: worker,
-        worker_count=1,
-        loop=loop,
-        db_host=db.host,
-        db_port=db.port,
-    ) as controller:
-
-        await db.put(app_updated)
-        await db.put(app_deleting)
-
-        await db.put(cluster_updated)
-        await db.put(cluster_deleting)
-
-        await asyncio.wait(
-            [controller, worker.done], timeout=3, return_when=asyncio.FIRST_COMPLETED
-        )
-    assert worker.done.done()
+    assert key_1 in (app_deleting.metadata.uid, cluster_deleting.metadata.uid)
+    assert key_2 in (app_deleting.metadata.uid, cluster_deleting.metadata.uid)
+    assert key_1 != key_2
 
 
-async def test_cluster_deletion(aiohttp_server, config, db, loop):
+async def test_cluster_deletion(config, db, loop):
     cluster = ClusterFactory(metadata__deleted=fake.date_time(tzinfo=pytz.utc))
 
     await db.put(cluster)
@@ -78,14 +60,11 @@ async def test_cluster_deletion(aiohttp_server, config, db, loop):
     for app in apps:
         await db.put(app)
 
-    server = await aiohttp_server(create_app(config))
-
+    # Ensure that the Applications are marked as deleted
     stored_apps = []
     for app in apps:
-        # Ensure that the Applications are marked as deleted
-        async with Client(url=server_endpoint(server), loop=loop) as client:
-            worker = GarbageWorker(client=client, db_host=db.host, db_port=db.port)
-            await worker.resource_received(cluster)
+        gc = GarbageCollector(db_host=db.host, db_port=db.port)
+        await gc.resource_received(cluster)
 
         stored_app = await db.get(
             Application, namespace=app.metadata.namespace, name=app.metadata.name
@@ -98,11 +77,10 @@ async def test_cluster_deletion(aiohttp_server, config, db, loop):
         await db.put(stored_app)
         stored_apps.append(stored_app)
 
+    # Ensure that the Application resources are deleted from database
     for app in stored_apps:
-        # Ensure that the Application resources are deleted from database
-        async with Client(url=server_endpoint(server), loop=loop) as client:
-            worker = GarbageWorker(client=client, db_host=db.host, db_port=db.port)
-            await worker.resource_received(app)
+        gc = GarbageCollector(db_host=db.host, db_port=db.port)
+        await gc.resource_received(app)
 
         stored_app = await db.get(
             Application,
