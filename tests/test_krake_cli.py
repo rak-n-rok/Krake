bbaa8161f8fa571bb31cb6d2d1e07f772d494a4a
Matthias Goerens <matthias.goerens@cloudandheat.com>
2019-10-11 14:53:57 +0200

    Add check of the command's return code in the smoke tests
    Signed-off-by: Matthias Goerens <matthias.goerens@cloudandheat.com>

--- tests/test_krake_cli.py
+++ tests/test_krake_cli.py
@@ -5,4 +5,5 @@ def test_list_krake_applications():
     cmd = "rok kube app list"
     response = util.run(cmd)
 
+    assert response.returncode == 0
     assert "name" in response.output
--- tests/test_minikube_cli.py
+++ tests/test_minikube_cli.py
@@ -5,4 +5,5 @@ def test_list_krake_applications():
     cmd = "kubectl get po --all-namespaces"
     response = util.run(cmd)
 
+    assert response.returncode == 0
     assert "NAMESPACE" in response.output
