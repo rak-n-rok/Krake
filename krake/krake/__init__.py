749cea10fee9908e449da93bfa4c562515c5c792
Jean Chorin <jean.chorin@cloudandheat.com>
2019-10-18 09:38:44 +0200

    Remove environment variable as config options
    See discussions on issue #266
    
    As the configuration files have been separated and the environment
    variables are not used anymore by our Docker compose, the support of the
    environment variables as configuration options is dropped. Thus the
    functions allowing this are simply removed.
    
    Instead, a feature will be added later on to add automatically
    command-line options for each configuration part.
    
    Signed-off-by: Jean Chorin <jean.chorin@cloudandheat.com>

--- krake/krake/__init__.py
+++ krake/krake/__init__.py
@@ -1,126 +1,7 @@
-import copy
 import os
 
 import yaml
 import logging.config
-from functools import reduce
-
-
-KRAKE_CONF_ENV_KEYS = (
-    "ETCD",
-    "TLS",
-    "AUTHENTICATION",
-    "AUTHORIZATION",
-    "CONTROLLERS",
-    "LOG",
-)
-
-
-def evaluate_value(value, base_value):
-    """Evaluate Krake environment variable value based on Krake base
-     configuration value and rules definition, see :func:``validate_krake_env`` doc
-
-    Args:
-        value: Krake environment variable value
-        base_value: Krake base configuration value
-
-    Raises:
-        ValueError if base_value type is not valid
-
-    Returns:
-        Evaluated Krake environment variable value
-
-    """
-    if isinstance(base_value, str):
-        return str(value)
-
-    if isinstance(base_value, bool):
-        return value in ("True", "true")
-
-    if isinstance(base_value, int):
-        return int(value)
-
-    raise ValueError
-
-
-def validate_krake_env(name, value, base_config):
-    """Validate Krake environment variable
-
-     Validation of Krake environment variable is defined by following rules:
-
-    1. Only environment variable name prefixed by the parent Krake
-    configuration key defined in ``KRAKE_CONF_ENV_KEYS`` can be loaded
-    2. Only environment variable name which strictly keeps the Krake base
-    configuration structure defined by ``base_config`` can be loaded
-    3. Only environment variable with non empty value can be loaded
-    4. Only <class 'str'> or <class 'int'> or <class 'bool'> value can be overwritten
-
-    Args:
-        name (str): Environment variable name
-        value (str): Environment variable value
-        base_config (dict): Krake base configuration
-
-    Raises:
-        KeyError: If environment variable name is not valid
-        ValueError: If environment variable value is empty
-
-    Returns:
-        list, str: Validated Krake configuration keys loaded from environment variable
-        and its value
-
-    """
-    if not name.startswith(KRAKE_CONF_ENV_KEYS):
-        raise KeyError
-
-    if not value.strip():
-        raise ValueError
-
-    keys = []
-    key_buffer = []
-    base_value = None
-
-    for key in [n.lower() for n in name.split("_")]:
-        base_key = "_".join(key_buffer + [key])
-        try:
-            base_value = reduce(lambda d, k: d[k], keys + [base_key], base_config)
-            keys.append(base_key)
-            key_buffer = []
-        except KeyError:
-            key_buffer.append(key)
-            continue
-
-    if not keys or "_".join(keys) != name.lower():
-        raise KeyError
-
-    value = evaluate_value(value, base_value)
-
-    return keys, value
-
-
-def load_env_config(base_config):
-    """Load Krake environment variables configuration
-
-    Selected Krake configuration values loaded from Krake YAML configuration file
-    can be overwritten by values loaded from environment variables.
-    Krake environment variable is validate by :func:`validate_krake_env`
-
-    Args:
-        base_config (dict): Krake base configuration
-
-    Returns:
-        dict: Krake environment variables configuration
-
-    """
-    env_config = {}
-    for name, value in os.environ.items():
-        try:
-            keys, value = validate_krake_env(name, value, base_config)
-        except (KeyError, ValueError):
-            continue
-
-        env_config[tuple(keys)] = value
-
-    return env_config
 
 
 def load_yaml_config(filepath):
@@ -154,13 +35,7 @@ def load_config(filepath):
         dict: Krake configuration
 
     """
-    base_config = load_yaml_config(filepath)
-    env_config = load_env_config(base_config)
-
-    config = copy.deepcopy(base_config)
-    for keys, value in env_config.items():
-        # Overwrite config value by value loaded from environment variable
-        reduce(lambda d, k: d[k], keys[:-1], config)[keys[-1]] = value
+    config = load_yaml_config(filepath=filepath)
 
     return config
 
--- krake/tests/api/test_config.py
+++ krake/tests/api/test_config.py
@@ -21,31 +21,3 @@ def test_config_load_base(config):
     config_loaded = load_config(config)
 
     assert config_loaded == config
-
-
-def test_config_overwrite(config, monkeypatch):
-    monkeypatch.setenv("TLS_ENABLED", "true")
-    config_loaded = load_config(config)
-
-    assert config_loaded["tls"]["enabled"] is True
-
-
-def test_config_prefix_restriction(config, monkeypatch):
-    monkeypatch.setenv("DEFAULT-ROLES", "true")
-    config_loaded = load_config(config)
-
-    assert config_loaded["default-roles"] == config["default-roles"]
-
-
-def test_config_empty_env_value(config, monkeypatch):
-    monkeypatch.setenv("TLS_ENABLED", "")
-    config_loaded = load_config(config)
-
-    assert config_loaded["tls"]["enabled"] == config["tls"]["enabled"]
-
-
-def test_config_underscore_param(config, monkeypatch):
-    monkeypatch.setenv("AUTHENTICATION_ALLOW_ANONYMOUS", "false")
-    config_loaded = load_config(config)
-
-    assert config_loaded["authentication"]["allow_anonymous"] is False
