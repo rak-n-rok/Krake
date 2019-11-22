b23522c292cc0659a6a01e35b4287991aeaaeeae
Lucas Kahlert <lucas.kahlert@cloudandheat.com>
2019-11-22 15:50:26 +0100

    Add tox for Python 3.6, 3.7 and 3.8
    Signed-off-by: Lucas Kahlert <lucas.kahlert@cloudandheat.com>

--- krake/setup.py
+++ krake/setup.py
@@ -30,6 +30,7 @@ setup(
     install_requires=install_requires,
     extras_require={
         "dev": {
+            "tox",
             "pytest",
             "pytest-aiohttp",
             "factory-boy",
