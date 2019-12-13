46f78e27b5ce605d7bddedebe965c60a69dcffc7
Matej Feder <matej.feder@x-works.io>
2019-12-13 18:06:06 +0100

    Add docker a bundle to create a docker infrastructure
    The infrastructure running Prometheus and metric exporter.
    
    Signed-off-by: Matej Feder <matej.feder@x-works.io>

--- docker/prometheus/metric_exporter/metric_exporter.py
+++ docker/prometheus/metric_exporter/metric_exporter.py
@@ -1,56 +1,101 @@
-from http import HTTPStatus
+"""Simple metrics exporter server exposes heat demand metrics also in minimal
+configuration suitable for end-to-end testing of Krake infrastructure.
 
-from flask import Flask
-from prometheus_client import Gauge, start_http_server
+Metrics exporter generates random heat demand metrics for multiple zones.
+Default number of zones is 5: `heat_demand_zone_1` .. `heat_demand_zone_5`.
+Random heat demand metric value is regenerated (each 10s) from interval :
+     <`zone_number` - 1, `zone_number`)
 
-PROMETHEUS_PORT = 9000
+.. code:: bash
 
-KRAKE_METRIC = Gauge('krake_metric', 'float or int - krake metric')
-HEAT_DEMAND = Gauge('heat_demand', 'float or int - heat demand (kW)')
-KRAKE_METRIC.set(0)
-HEAT_DEMAND.set(0)
+    $ python3 metric_exporter.py
+    $ curl -s curl localhost:9091/metrics
+    ...
+    # HELP heat_demand_zone_1 float - heat demand (kW)
+    # TYPE heat_demand_zone_1 gauge
+    heat_demand_zone_1 0.7
+    # HELP heat_demand_zone_2 float - heat demand (kW)
+    # TYPE heat_demand_zone_2 gauge
+    heat_demand_zone_2 1.22
+    # HELP heat_demand_zone_3 float - heat demand (kW)
+    # TYPE heat_demand_zone_3 gauge
+    heat_demand_zone_3 2.59
+    # HELP heat_demand_zone_4 float - heat demand (kW)
+    # TYPE heat_demand_zone_4 gauge
+    heat_demand_zone_4 3.81
+    # HELP heat_demand_zone_5 float - heat demand (kW)
+    # TYPE heat_demand_zone_5 gauge
+    heat_demand_zone_5 4.54
 
+"""
+import asyncio
+import functools
+import random
+from argparse import ArgumentParser
 
-class MetricMiddleware(object):
-
-    def __init__(self, application):
-        self.app = application
-        # Start http server in separate thread to expose metrics for consumption by
-        # prometheus
-        self.start_metrics_server()
-
-    @staticmethod
-    def start_metrics_server():
-        start_http_server(PROMETHEUS_PORT)
-
-    def __call__(self, environ, start_response):
-        return self.app(environ, start_response)
-
-
-app = Flask(__name__)
-app.wsgi_app = MetricMiddleware(app.wsgi_app)
-
-
-@app.route('/krake_metric/<float:value>', methods=['POST'])
-@app.route('/krake_metric/<int:value>', methods=['POST'])
-def update_krake_metric(value):
-    """
-    Handler for updating krake metric.
-    """
-    KRAKE_METRIC.set(value)
-    return '', HTTPStatus.OK
+from aiohttp import web
+from prometheus_async import aio
+from prometheus_client import Gauge
 
 
-@app.route('/heat_demand/<float:value>', methods=['POST'])
-@app.route('/heat_demand/<int:value>', methods=['POST'])
-def update_heat_demand(value):
-    """
-    Handler for updating heat demand.
-    """
-    HEAT_DEMAND.set(value)
-    return '', HTTPStatus.OK
+CYCLE_INTERVAL = 10
 
 
-if __name__ == '__main__':
+async def heat_demand_metric(zone):
+    metric = Gauge(f"heat_demand_zone_{zone}", "float - heat demand (kW)")
+    while True:
+        metric.set(round(random.uniform(zone - 1, zone), 2))
+        await asyncio.sleep(CYCLE_INTERVAL)
 
-    app.run(host='0.0.0.0', port=8080)
+
+async def start_metrics_tasks(zones, app):
+    app["metrics"] = [
+        asyncio.get_event_loop().create_task(heat_demand_metric(zone))
+        for zone in range(1, zones + 1)
+    ]
+
+
+async def cleanup_metrics_tasks(app):
+    for task in app["metrics"]:
+        task.cancel()
+    await app["metrics"]
+
+
+async def exporter(host, port, zones):
+
+    app = web.Application()
+    app.router.add_get("/metrics", aio.web.server_stats)
+    app.on_startup.append(functools.partial(start_metrics_tasks, zones))
+    app.on_cleanup.append(cleanup_metrics_tasks)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host, port)
+    await site.start()
+
+
+parser = ArgumentParser(
+    description="Metrics exporter server."
+)
+parser.add_argument(
+    "--host",
+    type=str,
+    default="0.0.0.0",
+    help="Exporter port to expose metrics. Default: 0.0.0.0",
+)
+parser.add_argument(
+    "--port",
+    type=int,
+    default=9091,
+    help="Exporter port to expose metrics. Default: 9091",
+)
+parser.add_argument(
+    "--zones", type=int, default=5, help="Head demand metrics zone count Default: 5"
+)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    loop = asyncio.get_event_loop()
+    loop.create_task(exporter(args.host, args.port, args.zones))
+    loop.run_forever()
