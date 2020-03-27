921db808499e122da3df06ba009541300d533e84
Jean Chorin <jean.chorin@cloudandheat.com>
2020-03-27 08:43:49 +0100

    Pinning major version of packages
    Signed-off-by: Jean Chorin <jean.chorin@cloudandheat.com>

--- rak/setup.py
+++ rak/setup.py
@@ -8,5 +8,5 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     install_requires=install_requires,
-    extras_require={"test": {"dataclasses==0.6.*", "docker", "pytest"}},
+    extras_require={"test": {"dataclasses==0.6.*", "docker==4.*", "pytest==5.*"}},
 )
