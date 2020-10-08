fc37211670fec67dc85c1461559fafed0571720a
Karin Fetzer <karin.fetzer@cloudandheat.com>
2020-10-08 15:44:48 +0200

    Remove frequent rescheduling by fluctuating metric
    When the scheduler has scheduled an application, this will
    cause the kubernetes controller to update the application,
    which will cause the scheduler to reconsider where to
    schedule the application.
    
    Previously, the scheduler would conclude to reschedule the
    application, if the metrics had changed so that it would be
    beneficial - independently of how recent the application had
    been scheduled.
    
    In the new implementation, the scheduler returns the current
    cluster (assuming it is still valid based on the cluster
    label constraints of the application), if it was less than
    Scheduler.reschedule_after seconds ago that the application
    was scheduled last.
    
    Signed-off-by: Karin Fetzer <karin.fetzer@cloudandheat.com>

--- krake/krake/controller/scheduler/scheduler.py
+++ krake/krake/controller/scheduler/scheduler.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import timedelta
 import logging
 import random
 from typing import NamedTuple
@@ -556,6 +557,42 @@ class Scheduler(Controller):
         if not matching:
             logger.info("No matching Kubernetes cluster for %r found", app)
             raise NoClusterFound("No matching Kubernetes cluster found")
+
+        # If the application already has been scheduled it might be that it
+        # was very recently. In fact, since the controller reacts to all updates
+        # of the application, it might very well be that the previous scheduling
+        # took place very recently. For example, the kubernetes controller updates
+        # the app in reaction to the scheduler's scheduling, in which case
+        # the scheduler will try and select the best cluster yet again.
+        # Therefore we have to make sure the previous scheduling was not too recent.
+        # If it was, we want to stay at the current cluster - if it is still matching.
+        # The above reasoning is also true in the case when the user has performed an
+        # update of the application's cluster label constraints. Also in this case, the
+        # update should not cause a migration if 'app was `recently scheduled`'
+        # and 'current cluster is still matching'. If the app was recently scheduled
+        # but the update caused the current cluster to no longer be matching, we
+        # will reschedule, since `current` will become None below.
+        # If 'app was NOT `recently scheduled`' and 'current cluster is still matching',
+        # we might reschedule, e.g., due to changing metrics.
+        if app.status.scheduled_to:
+            # get current cluster as first cluster in matching to which app is scheduled
+            current = next(
+                (c for c in matching if resource_ref(c) == app.status.scheduled_to),
+                None,
+            )
+            # if current cluster is still matching
+            if current:
+                # We check how long ago the previous scheduling took place.
+                # If it is less than reschedule_after seconds ago, we do not
+                # reschedule. We use reschedule_after for this comparison,
+                # since it indicates how often it is desired that an application
+                # should be rescheduled when a more appropriate cluster exists.
+                time_since_scheduled_to_current = utils.now() - app.status.scheduled
+                app_recently_scheduled = time_since_scheduled_to_current < timedelta(
+                    seconds=self.reschedule_after
+                )
+                if app_recently_scheduled:
+                    return current
 
         # Partition list if matching clusters into a list if clusters with
         # metrics and without metrics. Clusters with metrics are preferred
