b2ea1dbe97ad777f3aace6e666ea955bcadbdc1f
Hannes Baum <hannes.baum@cloudandheat.com>
2023-08-07 10:24:37 +0200

    Fix the pipeline errors from 07-08-23
    There were some problems with the Dockerfile that needed to be fixed.
    Especially the ownership of the logs directory made some problems, which are adapted in the integration tests and before now.
    
    Signed-off-by: Hannes Baum <hannes.baum@cloudandheat.com>

--- rak/functionals/test_krake_cli.py
+++ rak/functionals/test_krake_cli.py
@@ -4,6 +4,9 @@ from functionals.utils import run
 def test_list_krake_applications():
     cmd = "rok kube app list"
     response = run(cmd)
-
-    assert response.returncode == 0
+    try:
+        assert response.returncode == 0
+    except AssertionError:
+        print(response.output)
+        assert False
     assert "name" in response.output
