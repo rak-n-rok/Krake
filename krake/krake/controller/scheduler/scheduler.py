e1aa2082792868484f5861c25698a1f42439013d
Hannes Baum <hannes.baum@cloudandheat.com>
2023-12-19 13:15:15 +0100

    Revise scheduling for auto generated clusters
    Auto generated clusters shouldn't be scheduled to another cloud, since they were specifically created on a cloud.
    
    Signed-off-by: Hannes Baum <hannes.baum@cloudandheat.com>

--- krake/krake/controller/scheduler/scheduler.py
+++ krake/krake/controller/scheduler/scheduler.py
@@ -1408,26 +1408,29 @@ class KubernetesClusterHandler(Handler):
             )
             logging.info("Marked %r to be deleted.", cluster)
 
-        # Clouds from the same namespace as cluster are preferred over global clouds.
-        clouds_namespaced = await self.infrastructure_api.list_clouds(
-            namespace=cluster.metadata.namespace
-        )
-        try:
-            cloud = await self.select_cloud(cluster, clouds_namespaced.items)
-        except NoCloudFound:
-            # Try to find global clouds if there is no namespaced one.
-            clouds_global = await self.infrastructure_api.list_global_clouds()
-            cloud = await self.select_cloud(cluster, clouds_global.items)
+        # Only try to reschedule clusters that are not auto generated
+        if not cluster.spec.auto_generated:
+            # Clouds from the same namespace as the cluster
+            # are preferred over global clouds
+            clouds_namespaced = await self.infrastructure_api.list_clouds(
+                namespace=cluster.metadata.namespace
+            )
+            try:
+                cloud = await self.select_cloud(cluster, clouds_namespaced.items)
+            except NoCloudFound:
+                # Try to find global clouds if there is no namespaced one.
+                clouds_global = await self.infrastructure_api.list_global_clouds()
+                cloud = await self.select_cloud(cluster, clouds_global.items)
 
-        scheduled_to = resource_ref(cloud)
+            scheduled_to = resource_ref(cloud)
 
-        logger.info("Scheduled %r to %r", cluster, cloud)
+            logger.info("Scheduled %r to %r", cluster, cloud)
 
-        await self.kubernetes_api.update_cluster_binding(
-            namespace=cluster.metadata.namespace,
-            name=cluster.metadata.name,
-            body=CloudBinding(cloud=scheduled_to),
-        )
+            await self.kubernetes_api.update_cluster_binding(
+                namespace=cluster.metadata.namespace,
+                name=cluster.metadata.name,
+                body=CloudBinding(cloud=scheduled_to),
+            )
 
     @staticmethod
     def get_cloud_metrics(cloud):
