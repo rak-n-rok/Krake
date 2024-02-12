9d6259b6bbbd694d6bd9e4cc65a26370a8f93fd4
Bruno Schubert <bruno.schubert@cloudandheat.com>
2024-02-12 14:36:14 +0000

    hooks: Move hook dispatcher to central position
    Moves the hook dispatcher from the kubernetes controller to the base directory
     of all controllers so that the hooking mechanism may be used by all
     controllers,
    
    This is a prerequisite for the observer implementation of the infrastructure
     controller later on.
    
    Prerequisite-for: observer: Add `InfrastructureProviderClusterObserver`
    Signed-off-by: Bruno Schubert <bruno.schubert@cloudandheat.com>

---
+++ krake/krake/controller/hooks.py
@@ -0,0 +1,82 @@
+"""Module for the common controller hook mechanism
+
+Defines the HookDispatcher and listeners for registering and executing hooks.
+HookDispatcher emits hooks based on :class:`Hook` attributes which define when the hook
+will be executed.
+"""
+
+from collections import defaultdict
+from inspect import iscoroutinefunction
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class HookDispatcher(object):
+    """Simple wrapper around a registry of handlers associated to :class:`Hook`
+     attributes. Each :class:`Hook` attribute defines when the handler will be
+     executed.
+
+    Listeners for certain hooks can be registered via :meth:`on`. Registered
+    listeners are executed via :meth:`hook`.
+
+    Example:
+        .. code:: python
+
+        listen = HookDispatcher()
+
+        @listen.on(HookType.PreApply)
+        def to_perform_before_app_creation(app, cluster, resource, controller):
+            # Do Stuff
+
+        @listen.on(HookType.PostApply)
+        def another_to_perform_after_app_creation(app, cluster, resource, resp):
+            # Do Stuff
+
+        @listen.on(HookType.PostDelete)
+        def to_perform_after_app_deletion(app, cluster, resource, resp):
+            # Do Stuff
+
+    """
+
+    def __init__(self):
+        self.registry = defaultdict(list)
+
+    def on(self, hook):
+        """Decorator function to add a new handler to the registry.
+
+        Args:
+            hook (HookType): Hook attribute for which to register the handler.
+
+        Returns:
+            callable: Decorator for registering listeners for the specified
+            hook.
+
+        """
+
+        def decorator(handler):
+            self.registry[hook].append(handler)
+
+            return handler
+
+        return decorator
+
+    async def hook(self, hook, **kwargs):
+        """Execute the list of handlers associated to the provided :class:`Hook`
+        attribute.
+
+        Args:
+            hook (HookType): The hook attribute for which to execute handlers.
+
+        """
+        try:
+            handlers = self.registry[hook]
+        except KeyError:
+            pass
+        else:
+            for handler in handlers:
+                if iscoroutinefunction(handler):
+                    await handler(**kwargs)
+                else:
+                    handler(**kwargs)
