ee3cd1940755d08cd9e24d7d0572b4ace6ae9a33
Martin Morgenstern <martin.morgenstern@cloudandheat.com>
2024-02-14 11:44:32 +0100

    Remove unnecessary modifications of sys.path
    Signed-off-by: Martin Morgenstern <martin.morgenstern@cloudandheat.com>

--- krake/krake/api/__init__.py
+++ krake/krake/api/__init__.py
@@ -2,12 +2,4 @@
 implemented as an :mod:`aiohttp` application.
 """
 
-import os.path
-import sys
-
-parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, parent)
-
-from krake.__about__ import (  # noqa: E402
-    __version__
-)
+from krake.__about__ import __version__
