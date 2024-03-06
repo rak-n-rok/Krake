79daeb24db1927cdf8d60e83c67392c35804b94e
Bruno Schubert <bruno.schubert@cloudandheat.com>
2024-03-06 18:59:42 +0000

    unittest: (3) Test get methods of infra manager client
    Tests the previously added methods `get` and `get_vm` of the infrastructure manager
     client.
    
    Part-of: infra-provider: Add method to get infrastructure data
    Depends-on: infra-provider: Add method to get infrastructure data
    Depends-on: unittest: (1) Use separate dir for infra controller
    Signed-off-by: Bruno Schubert <bruno.schubert@cloudandheat.com>

--- krake/tests/controller/infrastructure/test_providers.py
+++ krake/tests/controller/infrastructure/test_providers.py
@@ -7,6 +7,11 @@ from random import randint
 import yaml
 
 from krake.client import Client
+from krake.data.infrastructure import (
+    InfrastructureProviderCluster,
+    InfrastructureProviderVm,
+    InfrastructureProviderVmCredential,
+)
 from krake.test_utils import server_endpoint
 
 from krake.controller.infrastructure import providers
@@ -566,3 +571,382 @@ async def test_InfrastuctureManager_get_kubeconfig(aiohttp_server, config, db, l
 
     # Assert that the kubeconfig was correctly requested from the infra manager
     infra_outputs_get_recorder.assert_called_once_with(content="")
+
+
+async def test_InfrastuctureManager_get_vm(aiohttp_server, config, db, loop):
+    """Test the `get_vm` method of the `InfrastructureManager` class.
+
+    Tests that the VM information is correctly retrieved from the
+    infrastructure manager (correct method, path and data).
+
+    Steps:
+        1. Mock infrastructure manager server
+           and inject infrastructure UUID, VM information
+           and attach API request recorder
+        2. Create infrastructure provider object
+        3. Create an openstack cloud object
+        4. Create a cluster
+        5. Create the infrastructure provider client from:
+           - a client session of the infrastructure manager server
+           - the openstack cloud object
+           - the infrastructure provider object
+        6. Run the target method `get_vm`
+
+    Asserts:
+        infrastructure manager client request (method, path, content, one call)
+        return value of :meth:`krake.controller.infrastructure.providers.
+            InfrastructureManager.get_vm`
+    """
+
+    # Mock the infrastructure manager API endpoints that will be called
+    # NOTE: Examples taken from a real infrastructure manager API response
+    #       and slightly modified
+    routes = web.RouteTableDef()
+    im_server_port = randint(30000, 50000)
+    infra_uuid = fake.uuid4()
+
+    vm_data_retrieval_recorder = mock.Mock()  # use mock object for recording
+
+    @routes.get(f"/infrastructures/{infra_uuid}/vms/0")
+    @record_aiohttp_requests(vm_data_retrieval_recorder)
+    async def _(request):
+        if request.headers['Accept'] == 'application/json':
+            return web.json_response(
+                {
+                    "radl": [
+                        {
+                            "class": "network",
+                            "id": "network_private",
+                            "cidr": "10.1.1.0/24",
+                            "create": "yes",
+                            "provider_id": f"im-{infra_uuid}-network_private",
+                        },
+                        {
+                            "class": "network",
+                            "id": "network_public",
+                            "outbound": "yes",
+                            "outports": "0.0.0.0/0-80/tcp-80/tcp,0.0.0.0/0-443/tcp-443/tcp",  # noqa: E501
+                            "provider_id": "shared-public-IPv4",
+                        },
+                        {
+                            "class": "system",
+                            "id": "front",
+                            "instance_name": "front-6e0882dc-d712-11ee-b2ec-e60cc0e9f147",  # noqa: E501
+                            "disk.0.os.flavour": "ubuntu",
+                            "disk.0.os.name": "linux",
+                            "disk.0.os.version": "22.04",
+                            "instance_type": "M",
+                            "memory.size": 4294967296,
+                            "cpu.count": 2,
+                            "net_interface.0.dns_name": "kubeserver",
+                            "net_interface.0.connection": "network_private",
+                            "net_interface.1.connection": "network_public",
+                            "cpu.arch": "x86_64",
+                            "disk.0.image.url": "ost://identity.example.com/802aecb9-7553-434d-a05c-cb8ce6103caa",  # noqa: E501
+                            "disk.0.free_size": 26843545600,
+                            "disk.0.os.credentials.username": "cloudadm",
+                            "disk.1.os.credentials.username": "user2",
+                            "provider.type": "OpenStack",
+                            "provider.host": "identity.example.com",
+                            "provider.port": 5000,
+                            "disk.0.os.credentials.private_key": "PRIVATE_KEY-8x9Am",
+                            "disk.1.os.credentials.password": "PASSWORD-3m91x",
+                            "disk.1.os.credentials.private_key": "PRIVATE_KEY-d92sp",
+                            "state": "running",
+                            "instance_id": "5c205fb4-b1e0-4e78-ae1e-333ea18c1a27",
+                            "net_interface.0.ip": "192.0.2.3",
+                            "net_interface.1.ip": "203.0.113.3",
+                        }
+                    ]
+                }
+            )
+
+    # Create infrastructure manager server with the mocked API endpoint
+    im_app = web.Application()
+    im_app.add_routes(routes)
+    im_server = await aiohttp_server(im_app, port=im_server_port)
+
+    # Create the required Krake resources in the Krake database
+    infra_provider = InfrastructureProviderFactory(
+        spec__type="im",
+        spec__im__url=server_endpoint(im_server),
+    )
+    cloud = CloudFactory(spec__type="openstack")
+    cluster = ClusterFactory(status__cluster_id=infra_uuid)
+    # Not creating an infrastructure provider binding as it is not needed here
+
+    async with Client(url=server_endpoint(im_server), loop=loop) as client:
+        # Create an infrastructure provider client
+        infra_provider_client = providers.InfrastructureProvider(
+            session=client.session,
+            cloud=cloud,
+            infrastructure_provider=infra_provider
+        )
+
+        # Run target method
+        returned = await infra_provider_client.get_vm(cluster, 0)
+
+    # Assert that the target method returned the expected vm data object
+    assert returned == InfrastructureProviderVm(
+        name="front-6e0882dc-d712-11ee-b2ec-e60cc0e9f147",
+        ip_addresses=[
+            "192.0.2.3",
+            "203.0.113.3",
+        ],
+        credentials=[
+            InfrastructureProviderVmCredential(
+                username="cloudadm",
+                password=None,
+                private_key="PRIVATE_KEY-8x9Am",
+            ),
+            InfrastructureProviderVmCredential(
+                username="user2",
+                password="PASSWORD-3m91x",
+                private_key="PRIVATE_KEY-d92sp",
+            ),
+        ],
+    )
+
+    # Assert that the kubeconfig was correctly requested from the infra manager
+    vm_data_retrieval_recorder.assert_called_once_with(content="")
+
+
+async def test_InfrastuctureManager_get(aiohttp_server, config, db, loop):
+    """Test the `get` method of the `InfrastructureManager` class.
+
+    Tests that cluster information is correctly retrieved from the
+    infrastructure manager (correct calls and data).
+
+    Steps:
+        1. Mock infrastructure manager server
+           and inject infrastructure UUID, VM information
+           and attach API request recorder
+        2. Create infrastructure provider object
+        3. Create an openstack cloud object
+        4. Create a cluster
+        5. Create the infrastructure provider client from:
+           - a client session of the infrastructure manager server
+           - the openstack cloud object
+           - the infrastructure provider object
+        6. Run the target method `get`
+
+    Asserts:
+        infrastructure manager client requests (methods, paths, contents, call order)
+        return value of :meth:`krake.controller.infrastructure.providers.
+            InfrastructureManager.get`
+    """
+
+    # Mock the infrastructure provider API endpoints that will be called
+    # NOTE: Examples taken from real infrastructure provider API responses and slightly
+    #       altered for more variance and generality.
+
+    routes = web.RouteTableDef()
+    im_server_port = randint(30000, 50000)
+    infra_uuid = fake.uuid4()
+
+    infra_data_retrieval_recorder = mock.Mock()  # use mock object for recording
+
+    @routes.get(f"/infrastructures/{infra_uuid}/vms/0")
+    @record_aiohttp_requests(
+        infra_data_retrieval_recorder,
+        endpoint=f"/infrastructures/{infra_uuid}/vms/0", method="get")
+    async def _(request):
+        if request.headers['Accept'] == 'application/json':
+            return web.json_response(
+                {
+                    "radl": [
+                        {
+                            "class": "network",
+                            "id": "network_private",
+                            "cidr": "10.1.1.0/24",
+                            "create": "yes",
+                            "provider_id": f"im-{infra_uuid}-network_private",
+                        },
+                        {
+                            "class": "network",
+                            "id": "network_public",
+                            "outbound": "yes",
+                            "outports": "0.0.0.0/0-80/tcp-80/tcp,0.0.0.0/0-443/tcp-443/tcp",  # noqa: E501
+                            "provider_id": "shared-public-IPv4",
+                        },
+                        {
+                            "class": "system",
+                            "id": "front",
+                            "instance_name": "front-6e0882dc-d712-11ee-b2ec-e60cc0e9f147",  # noqa: E501
+                            "disk.0.os.flavour": "ubuntu",
+                            "disk.0.os.name": "linux",
+                            "disk.0.os.version": "22.04",
+                            "instance_type": "M",
+                            "memory.size": 4294967296,
+                            "cpu.count": 2,
+                            "net_interface.0.dns_name": "kubeserver",
+                            "net_interface.0.connection": "network_private",
+                            "net_interface.1.connection": "network_public",
+                            "cpu.arch": "x86_64",
+                            "disk.0.image.url": "ost://identity.example.com/802aecb9-7553-434d-a05c-cb8ce6103caa",  # noqa: E501
+                            "disk.0.free_size": 26843545600,
+                            "disk.0.os.credentials.username": "cloudadm",
+                            "provider.type": "OpenStack",
+                            "provider.host": "identity.example.com",
+                            "provider.port": 5000,
+                            "disk.0.os.credentials.private_key": "PRIVATE_KEY-8x9Am",
+                            "state": "running",
+                            "instance_id": "5c205fb4-b1e0-4e78-ae1e-333ea18c1a27",
+                            "net_interface.0.ip": "192.0.2.3",
+                            "net_interface.1.ip": "203.0.113.3",
+                        }
+                    ]
+                }
+            )
+
+    @routes.get(f"/infrastructures/{infra_uuid}/vms/1")
+    @record_aiohttp_requests(
+        infra_data_retrieval_recorder,
+        endpoint=f"/infrastructures/{infra_uuid}/vms/1", method="get")
+    async def _(request):
+        if request.headers['Accept'] == 'application/json':
+            return web.json_response(
+                {
+                    "radl": [
+                        {
+                            "class": "network",
+                            "id": "network_private",
+                            "cidr": "10.1.1.0/24",
+                            "create": "yes",
+                            "provider_id": f"im-{infra_uuid}-network_private",
+                        },
+                        {
+                            "class": "network",
+                            "id": "network_public",
+                            "outbound": "yes",
+                            "outports": "0.0.0.0/0-80/tcp-80/tcp,0.0.0.0/0-443/tcp-443/tcp",  # noqa: E501
+                            "provider_id": "shared-public-IPv4",
+                        },
+                        {
+                            "class": "system",
+                            "id": "front",
+                            "instance_name": "wn-6e04627e-d712-11ee-b5fb-e60cc0e9f147",
+                            "disk.0.os.flavour": "ubuntu",
+                            "disk.0.os.name": "linux",
+                            "disk.0.os.version": "20.04",
+                            "instance_type": "M",
+                            "memory.size": 4294967296,
+                            "cpu.count": 2,
+                            "net_interface.0.dns_name": "kubeserver",
+                            "net_interface.0.connection": "network_private",
+                            "net_interface.1.connection": "network_public",
+                            "cpu.arch": "x86_64",
+                            "disk.0.image.url": "ost://identity.example.com/802aecb9-7553-434d-a05c-cb8ce6103caa",  # noqa: E501
+                            "disk.0.free_size": 26843545600,
+                            "disk.0.os.credentials.username": "user1",
+                            "disk.1.os.credentials.username": "user2",
+                            "provider.type": "OpenStack",
+                            "provider.host": "identity.example.com",
+                            "provider.port": 5000,
+                            "disk.0.os.credentials.private_key": "PRIVATE_KEY-B35r0",
+                            "disk.0.os.credentials.password": "PASSWORD-sl6r",
+                            "disk.1.os.credentials.password": "PASSWORD-s8c1",
+                            "state": "running",
+                            "instance_id": "009f9f39-b2ef-49a2-8cb0-437abbbe328f",
+                            "net_interface.0.ip": "192.0.2.4",
+                            "net_interface.1.ip": "203.0.113.4",
+                        }
+                    ]
+                }
+            )
+
+    @routes.get(f"/infrastructures/{infra_uuid}")
+    @record_aiohttp_requests(
+        infra_data_retrieval_recorder,
+        endpoint=f"/infrastructures/{infra_uuid}", method="get")
+    async def _(request):
+        if request.headers['Accept'] == 'application/json':
+            return web.json_response(
+                {
+                    "uri-list": [
+                        {"uri": f"http://localhost:{im_server_port}/infrastructures/{infra_uuid}/vms/0"},  # noqa: E501
+                        {"uri": f"http://localhost:{im_server_port}/infrastructures/{infra_uuid}/vms/1"},  # noqa: E501
+                    ]
+                }
+            )
+
+    # Create infrastructure manager server with the mocked API endpoints
+    # NOTE: Infrastructure manager is mocked here
+    #       because we are dealing with a cloud of type "openstack"
+    im_app = web.Application()
+    im_app.add_routes(routes)
+    im_server = await aiohttp_server(im_app, port=im_server_port)
+
+    # Create the required Krake resources in the Krake database
+    infra_provider = InfrastructureProviderFactory(
+        spec__type="im",
+        spec__im__url=server_endpoint(im_server),
+    )
+    cloud = CloudFactory(spec__type="openstack")
+    cluster = ClusterFactory(status__cluster_id=infra_uuid)
+    # Not creating an infrastructure provider binding as it is not needed here
+
+    async with Client(url=server_endpoint(im_server), loop=loop) as client:
+        # Create an infrastructure provider client
+        infra_provider_client = providers.InfrastructureProvider(
+            session=client.session,
+            cloud=cloud,
+            infrastructure_provider=infra_provider
+        )
+
+        # Run target method
+        returned = await infra_provider_client.get(cluster)
+
+    # Assert that the target method returned the expected data object
+    assert returned == InfrastructureProviderCluster(
+        id=infra_uuid,
+        vms=[
+            InfrastructureProviderVm(
+                name="front-6e0882dc-d712-11ee-b2ec-e60cc0e9f147",
+                ip_addresses=["192.0.2.3", "203.0.113.3"],
+                credentials=[
+                    InfrastructureProviderVmCredential(
+                        username="cloudadm",
+                        password=None,
+                        private_key="PRIVATE_KEY-8x9Am",
+                    ),
+                ],
+            ),
+            InfrastructureProviderVm(
+                name="wn-6e04627e-d712-11ee-b5fb-e60cc0e9f147",
+                ip_addresses=["192.0.2.4", "203.0.113.4"],
+                credentials=[
+                    InfrastructureProviderVmCredential(
+                        username="user1",
+                        password="PASSWORD-sl6r",
+                        private_key="PRIVATE_KEY-B35r0",
+                    ),
+                    InfrastructureProviderVmCredential(
+                        username="user2",
+                        password="PASSWORD-s8c1",
+                        private_key=None,
+                    ),
+                ],
+            ),
+        ],
+    )
+
+    # Assert that the cluster data was correctly requested from the infra manager
+    # using the expected calls
+    infra_data_retrieval_recorder.assert_has_calls(
+        calls=[
+            mock.call(
+                endpoint=f"/infrastructures/{infra_uuid}", method="get",
+                content="",
+            ),
+            mock.call(
+                endpoint=f"/infrastructures/{infra_uuid}/vms/0", method="get",
+                content="",
+            ),
+            mock.call(
+                endpoint=f"/infrastructures/{infra_uuid}/vms/1", method="get",
+                content="",
+            ),
+        ],
+        any_order=False,
+    )
