b8c593035801e20922838bf2ac22d5d755f40cba
Theresa Schüttig <theresa.schuettig@cloudandheat.com>
2024-03-20 15:10:18 +0100

    refactoring
    Signed-off-by: Theresa Schüttig <theresa.schuettig@cloudandheat.com>

--- krake/tests/controller/kubernetes/application/test_application_controller_shutdown.py
+++ krake/tests/controller/kubernetes/application/test_application_controller_shutdown.py
@@ -1,6 +1,5 @@
+from asyncio import AbstractEventLoop
 from copy import deepcopy
-from typing import Any
-from unittest.mock import Mock
 from mock import AsyncMock, patch
 import pytest
 
@@ -19,7 +18,26 @@ from tests.controller.kubernetes import (
     nginx_manifest
 )
 
+
 # region Arrangement
+@pytest.fixture(scope='function')
+def controller(loop: AbstractEventLoop) -> KubernetesApplicationController:
+    cluster = ClusterFactory()
+
+    kubernetes_api_mock: KubernetesApi = AsyncMock()
+    kubernetes_api_mock.read_application.return_value = app_shutdown_hook
+    kubernetes_api_mock.read_cluster.return_value = cluster
+    kubernetes_api_mock.update_application_status.return_value = \
+        ApplicationFactory(status__state=ApplicationState.WAITING_FOR_CLEANING)
+
+    controller = KubernetesApplicationController(
+        "http://krake.api", loop=loop
+    )
+
+    controller.kubernetes_api = kubernetes_api_mock
+    return controller
+
+
 @pytest.fixture
 def app_shutdown_hook() -> Application:
     """ creates application as specified in the nginx_manifest
@@ -35,95 +53,108 @@ def app_shutdown_hook() -> Application:
         spec__shutdown_grace_time=0
     )
 
+
 def get_delete_async_patch():
     return patch("krake.controller.kubernetes.client.KubernetesClient.delete_async")
 # endregion Arrangement
 
-# region Unit tests
+
+@patch.object(KubernetesClient, 'delete_async')
+@patch.object(KubernetesClient, 'shutdown_async')
 async def test_shutdown_failure_give_up(
-        app_shutdown_hook: Application,
-        hooks_config: HooksConfiguration,
-        ):
+    shutdown_async: AsyncMock,
+    delete_async: AsyncMock,
+    app_shutdown_hook: Application,
+    hooks_config: HooksConfiguration,
+    controller: KubernetesApplicationController
+):
     """ Enusure strategy 'give_up' calls shutdown and does not delete any ressources
     """
 
     hooks_config.shutdown.failure_strategy = ShutdownHookFailureStrategy.GIVE_UP.value
     hooks_config.shutdown.failure_retry_count = 0
-    controller = KubernetesApplicationController("http://krake.api", hooks=hooks_config)
 
-    kubernetes_client_mock : KubernetesClient = Mock()
-    with get_delete_async_patch() as delete_async_patch:
-        await controller._run_app_shutdown_strategy_async(kubernetes_client_mock,
-                                                              app_shutdown_hook)
-        delete_async_patch.assert_not_called()
+    controller.hooks = hooks_config
 
-    kubernetes_client_mock.shutdown_async.assert_called_once()
+    await controller._shutdown_task_async(app_shutdown_hook)
+    delete_async.assert_not_called()
+
+    shutdown_async.assert_called_once()
 
 
+@patch.object(KubernetesClient, 'delete_async')
+@patch.object(KubernetesClient, 'shutdown_async')
 async def test_shutdown_failure_delete(
+    shutdown_async: AsyncMock,
+    delete_async: AsyncMock,
     app_shutdown_hook: Application,
-    hooks_config: HooksConfiguration
+    hooks_config: HooksConfiguration,
+    controller: KubernetesApplicationController
 ):
     """ Enusure strategy delete calls shutdown and deletes all ressources of the
       application
       """
-    
+
     hooks_config.shutdown.failure_strategy = ShutdownHookFailureStrategy.DELETE.value
     hooks_config.shutdown.failure_retry_count = 0
-    
-    kubernetes_client_mock: KubernetesClient = AsyncMock()
 
-    cluster = ClusterFactory()
+    controller.hooks = hooks_config
 
-    kubernetes_api_mock: KubernetesApi = AsyncMock()
-    kubernetes_api_mock.read_application.return_value = app_shutdown_hook
-    kubernetes_api_mock.read_cluster.return_value = cluster
-    kubernetes_api_mock.update_application_status.return_value = \
-        ApplicationFactory(status__state=ApplicationState.WAITING_FOR_CLEANING)
+    await controller._shutdown_task_async(app_shutdown_hook)
 
+    # check that all manifests of the application were deleted
+    assert delete_async.call_count == len(nginx_manifest)
 
-    controller = KubernetesApplicationController(
-        "http://krake.api",
-        hooks=hooks_config,
-    )
+    for call_args in delete_async.call_args_list:
+        call_args.args[0] in nginx_manifest
 
-    controller.kubernetes_api = kubernetes_api_mock
-
-    with get_delete_async_patch() as delete_async_patch:
-        await controller._run_app_shutdown_strategy_async(
-            kubernetes_client_mock,
-            app_shutdown_hook)
-        
-        # check that all manifests of the application were deleted
-        assert delete_async_patch.call_count == len(nginx_manifest)
-
-        for call_args in delete_async_patch.call_args_list:
-            call_args.args[0] in nginx_manifest
-
-    
-    kubernetes_client_mock.shutdown_async.assert_called_once()
+    shutdown_async.assert_called_once()
     assert app_shutdown_hook.status.state == ApplicationState.DELETING
 
 
+@patch.object(KubernetesClient, 'delete_async')
+@patch.object(KubernetesClient, 'shutdown_async')
 async def test_shutdown_failure_retries(
-        app_shutdown_hook: Application,
-        hooks_config: HooksConfiguration,
-        ):
-    """ Enusure strategy shutdown is shutdown is attempted as often as specified retry count
+    shutdown_async: AsyncMock,
+    delete_async: AsyncMock,
+    app_shutdown_hook: Application,
+    hooks_config: HooksConfiguration,
+    controller: KubernetesApplicationController
+):
+    """ Enusure strategy shutdown is shutdown is attempted as often as specified retry
+    count
     """
-    
+
     retry_count = 3
     hooks_config.shutdown.failure_strategy = ShutdownHookFailureStrategy.GIVE_UP.value
     hooks_config.shutdown.failure_retry_count = retry_count
 
-    controller = KubernetesApplicationController("http://krake.api", hooks=hooks_config)
+    controller.hooks = hooks_config
 
-    kubernetes_client_mock : KubernetesClient = Mock()
-    with get_delete_async_patch() as delete_async_patch:
-        await controller._run_app_shutdown_strategy_async(kubernetes_client_mock,
-                                                              app_shutdown_hook)
-        delete_async_patch.assert_not_called()
+    await controller._shutdown_task_async(app_shutdown_hook)
+    delete_async.assert_not_called()
 
-    assert kubernetes_client_mock.shutdown_async.call_count == retry_count + 1
+    assert shutdown_async.call_count == retry_count + 1
+
+
+@patch.object(KubernetesClient, 'shutdown_async')
+@pytest.mark.parametrize("app_state", [ApplicationState.FAILED, ApplicationState.DEGRADED])
+async def test_shutdown_failed_state(
+    shutdown_async: AsyncMock,
+    app_state,
+    app_shutdown_hook,
+    hooks_config,
+    controller: KubernetesApplicationController
+):
+    """ Ensure no shutdown request is sent to application if has failed or degraded as
+    application state
+    """
+
+    app_shutdown_hook.status.state = app_state
+    controller.hooks = hooks_config
+
+    await controller._shutdown_task_async(app_shutdown_hook)
+
+    shutdown_async.assert_not_called()
+
 # endregion Unit tests
-    
