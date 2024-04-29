3a8adb5f0c7255f51954e978a4885fa08ddb6f42
Hannes Baum <hannes.baum@cloudandheat.com>
2024-04-29 14:28:02 +0200

    Add towncrier and release files
    Adds the towncrier package, release note templates and pipeline steps to build release notes.
    
    Signed-off-by: Hannes Baum <hannes.baum@cloudandheat.com>

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "_releasenotes", "Thumbs.db", ".DS_Store"]
 
 # The document name of the "master" document, that is, the document that
 # contains the root toctree directive.
